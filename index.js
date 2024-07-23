ab2860af8fb9beadcb215d3524de7a36ab6e8de4
lhmuana <edd_vai@yahoo.co.in>
2024-07-23 12:54:28 +0530

    malu leh kalu update

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function speakText(text) {
     const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voiceURI = 'Google हिन्दी';
     utterance.lang = 'hi-IN'; // Set the language to Hindi (India)
+    utterance.rate = 0.7;
     speechSynthesis.speak(utterance);
+
+    const voices = speechSynthesis.getVoices();
+    
+    // Find a female voice for Hindi (India) if available
+    const femaleVoice = voices.find(voice => voice.lang === 'hi-IN' && voice.name.includes('Female'));
+    console.log(femaleVoice);
 }
